01456af2d77349dc846a044519176e82ee604d17
xiaoyang zhao <zhaocnus@gmail.com>
2016-01-26 10:57:49 -0800

    changed ; to &&

--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node data_processing/main-scrape-app-ids.js;
-node data_processing/main-scrape-app-details.js;
-node data_processing/main-process-app-icons.js;
-node data_processing/main-generate-web-save-colors.js;
+node data_processing/main-scrape-app-ids.js &&
+node data_processing/main-scrape-app-details.js &&
+node data_processing/main-process-app-icons.js &&
+node data_processing/main-generate-web-save-colors.js
